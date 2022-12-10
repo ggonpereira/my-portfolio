58df7f838ce0339a21250710a2af4cbd34aeb856
Gabriel Pereira <ggonpereira@gmail.com>
2022-12-10 20:10:29 -0300

    💎 style: add Fira Code as default font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
+import '../styles/_app.css'
 import type { AppProps } from 'next/app'
+import { GlobalStyle } from '../styles/global'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
---
+++ styles/global.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    padding: 0px;
+    margin: 0px;
+    font-family: "Fira Code", sans-serif;
+  }
+`
